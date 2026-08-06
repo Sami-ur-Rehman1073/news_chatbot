a10e0b3a613c26703dd19f2f265cc8385bcf274d
Sami <sami.rehman1073@gmail.com>
2026-08-06 16:34:04 +0500

    Added the edges for new nodes of duplication detetction

--- graph/builder.py
+++ graph/builder.py
@@ -1,50 +1,106 @@
-from langgraph.graph import START, END, StateGraph
+from langgraph.graph import StateGraph, START, END
 
 from graph.state import GraphState
-
 from graph.nodes import (
     user_input_node,
     topic_extraction_node,
+    query_refinement_node,
     duckduckgo_search_node,
     newsapi_search_node,
     gnews_search_node,
     merge_results_node,
-    article_summarization_node
+    article_summarization_node,
+    duplicate_detection_node,
+    duplicate_filter_node,
 )
 
 
 def build_graph():
 
-    builder = StateGraph(GraphState)
+    workflow = StateGraph(GraphState)
 
     # Nodes
-    builder.add_node("user_input", user_input_node)
-    builder.add_node("topic_extraction", topic_extraction_node)
+    workflow.add_node("user_input", user_input_node)
+    workflow.add_node("topic_extraction", topic_extraction_node)
+    workflow.add_node("query_refinement", query_refinement_node)
 
-    builder.add_node("duckduckgo_search", duckduckgo_search_node)
-    builder.add_node("newsapi_search", newsapi_search_node)
-    builder.add_node("gnews_search", gnews_search_node)
+    workflow.add_node("duckduckgo_search", duckduckgo_search_node)
+    workflow.add_node("newsapi_search", newsapi_search_node)
+    workflow.add_node("gnews_search", gnews_search_node)
 
-    builder.add_node("merge_results", merge_results_node)
-    builder.add_node("article_summarization", article_summarization_node)
+    workflow.add_node("merge_results", merge_results_node)
 
-    # Workflow
-    builder.add_edge(START, "user_input")
-    builder.add_edge("user_input", "topic_extraction")
+    workflow.add_node(
+        "article_summarization",
+        article_summarization_node
+    )
+
+    workflow.add_node(
+        "duplicate_detection",
+        duplicate_detection_node
+    )
+
+    workflow.add_node(
+        "duplicate_filter",
+        duplicate_filter_node
+    )
+
+    # Initial flow
+    workflow.add_edge(START, "user_input")
+    workflow.add_edge("user_input", "topic_extraction")
+    workflow.add_edge("topic_extraction", "query_refinement")
 
     # Parallel search
-    builder.add_edge("topic_extraction", "duckduckgo_search")
-    builder.add_edge("topic_extraction", "newsapi_search")
-    builder.add_edge("topic_extraction", "gnews_search")
+    workflow.add_edge(
+        "query_refinement",
+        "duckduckgo_search"
+    )
 
-    # Merge
-    builder.add_edge("duckduckgo_search", "merge_results")
-    builder.add_edge("newsapi_search", "merge_results")
-    builder.add_edge("gnews_search", "merge_results")
+    workflow.add_edge(
+        "query_refinement",
+        "newsapi_search"
+    )
 
-    # Summarization
-    builder.add_edge("merge_results", "article_summarization")
+    workflow.add_edge(
+        "query_refinement",
+        "gnews_search"
+    )
 
-    builder.add_edge("article_summarization", END)
+    # Merge after all searches complete
+    workflow.add_edge(
+        "duckduckgo_search",
+        "merge_results"
+    )
 
-    return builder.compile()
+    workflow.add_edge(
+        "newsapi_search",
+        "merge_results"
+    )
+
+    workflow.add_edge(
+        "gnews_search",
+        "merge_results"
+    )
+
+    # Remaining pipeline
+    workflow.add_edge(
+        "merge_results",
+        "article_summarization"
+    )
+
+    workflow.add_edge(
+        "article_summarization",
+        "duplicate_detection"
+    )
+
+    workflow.add_edge(
+        "duplicate_detection",
+        "duplicate_filter"
+    )
+
+    workflow.add_edge(
+        "duplicate_filter",
+        END
+    )
+
+    return workflow.compile()
